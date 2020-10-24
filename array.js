88be3d0380d46e715973742180437da9353cd8e8
wcsomega <wcsomega@gmail.com>
2020-10-24 01:12:41 -0400

    Create array wrapper type.

--- array.js
+++ array.js
@@ -1,6 +1,16 @@
+function __Array__ (__value__) {
+  this['@@value@@'] = __value__;
+  this.toString = () => __value__.toString();
+  this['@@mappend@@'] = b => new __Array__(__value__.concat(value(b)));
+  this['@@fmap@@'] = b => new __Array__(__value__.map(b));
+  this['@@fromPure@@'] = a => new __Array__ ([a]);
+  this['@@fromEmpty@@'] = () => new __Array__ ([]);
+  this['@@chain@@'] = undefined;
+  this['@@ap@@'] = undefined;
+}
+
+const value = a => a['@@value@@'];
+
 module.exports = {
-  array$foldr: func => init => arr => arr.reduceRight((a, v) => func(a)(v), init),
-  array$mappend: a => b => a.concat(b),
-  array$empty: () => [],
-  array$of: (a) => [a],
+  tryWrapArray: (a) => a instanceof Array ? new __Array__(a) : a,
 }
