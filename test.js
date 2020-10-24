8716303b356f289a081d6db257983eac49b864fd
wcsomega <wcsomega@gmail.com>
2020-10-24 01:19:18 -0400

    Testing equality and comparison functions.

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const { right, left, isRight } = require('./either');
 const { mappend } = require('./mappend');
 const { empty } = require('./empty');
 const { pure } = require('./pure');
+const { eq, lte } = require('./equals');
 
 const add = a => b => a + b;
 const mul = a => b => a * b;
@@ -12,9 +13,6 @@ const mod = a => b => b % a;
 const neg = mul(-1);
 const inc = add(1);
 const dec = sub(1);
-
-//setoid
-const equals = a => b => a['@@equals@@'](b);
 
 //functor
 const fmap = f => a => a['@@fmap@@'](f);
@@ -33,6 +31,12 @@ const assoc = p => value => obj => Object.assign(obj, {[p]: value});
 const dissoc = p => obj => {let r = {...obj}; delete r[p]; return r};
 const safeAssoc = p => ma => o => maybe(flip(assoc(p))(o))(always(o))(ma);
 
+//functions on strings
+const lines = a => a.split('\n');
+const unlines = a => a.join('\n');
+const words = a => a.split(/\s+/);
+const unwords = a => a.join(' ');
+
 //parse
 const safeParseInt = radix => string => {let result = parseInt(string, radix); return isNaN(result) ? nothing() : some(result)};
 
@@ -40,6 +44,7 @@ const safeParseInt = radix => string => {let result = parseInt(string, radix); r
 const pipe = funcs => val => funcs.reduce((a, v) => v(a), val);
 const compose = funcs => val => funcs.reduceRight((a, v) => v(a), val);
 const applyTo = a => func => func(a);
+const $ = a => func => b => func(a)(b); //infix function
 const flip = f => a => b => f(b)(a);
 const always = x => () => x;
 
@@ -53,10 +58,12 @@ const foldr = func => init => arr => arr.reduceRight((a, v) => func(v)(a), init)
 // mconcat(['a', 'b', 'c']) = 'abc'
 const mconcat = foldr(mappend)(empty());
 
-let result = mconcat([
-  some([1]),
-  some([2]),
-  some([3]),
-])
+const not = a => !a;
 
-print(result);
+let complement = func => compose([not, func]);
+let neq = compose([complement, eq]);
+let gt = compose([complement, lte]);
+let lt = a => b => lte(a)(b) && neq(a)(b);
+let gte = a => b => gt(a)(b) || eq(a)(b);
+
+print(gte(some(1))(some(1)));
