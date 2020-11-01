fd176c3f3802b5bc24d74715e38a86462683199d
wcsomega <wcsomega@gmail.com>
2020-11-01 01:29:34 -0500

    Add pluck and pick functions. Remove functions that are now in separate modules.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const { some, nothing, maybe, isSome } = require('./maybe');
-const { mappend } = require('./mappend');
-const { empty } = require('./empty');
-const { eq, lte } = require('./EqOrd');
-const { foldr } = require('./array');
+const { some, nothing, maybe } = require('./maybe');
+const { eq } = require('./EqOrd');
+const { pipe, id } = require('./function');
+const { tryWrapArray } = require('./array');
 
 //functor
 const fmap = f => a => a['@@fmap@@'](f);
@@ -13,25 +12,20 @@ const lift2 = f => a => ap(fmap(f)(a));
 const lift3 = f => a => b => ap(ap(fmap(f)(a))(b));
 
 //monad
-const chain = (f) => (a) => a['@@chain@@'](f);
+const chain = f => a => a['@@chain@@'](f);
+const join = chain(id);
 
 //object
-const prop = p => obj =>  p in obj ? some(obj[p]) : nothing();
+const prop = p => obj => p in obj ? some(obj[p]) : nothing();
+const pick = ps => obj => ps.reduce((a, v) => v in obj ? {...a, [v]: obj[v]} : a, {});
 const propEq = p => val => pipe([prop(p), eq(some(val))]);
 const assoc = p => value => obj => Object.assign(obj, {[p]: value});
 const dissoc = p => obj => {let r = {...obj}; delete r[p]; return r};
 const safeAssoc = p => ma => o => maybe(flip(assoc(p))(o))(always(o))(ma);
+const pluck = p => fmap(prop(p));
 
 //parse
 const safeParseInt = radix => string => {let result = parseInt(string, radix); return isNaN(result) ? nothing() : some(result)};
-
-//functions
-const pipe = funcs => val => funcs.reduce((a, v) => v(a), val);
-const compose = funcs => val => funcs.reduceRight((a, v) => v(a), val);
-const applyTo = a => func => func(a);
-const $ = a => func => b => func(a)(b); //infix function
-const flip = f => a => b => f(b)(a);
-const always = x => () => x;
 
 const ifElse = iftrue => iffalse => a => a ? iftrue() : iffalse();
 
@@ -39,14 +33,16 @@ const print = x => console.log(x.toString());
 const filter = pred => arr => arr.filter(pred);
 const reject = pred => arr => arr.filter(complement(pred));
 
+const objs = tryWrapArray([
+  {a: 1, b: 2, c: 1},
+  {a: 1, b: 4, c: 4},
+  {a: 2, b: 6, c: 9},
+  {a: 3, b: 8, c: 16},
+  {a: 5, b: 10, c: 25},
+  {a: 8, b: 12, c: 36},
+  {a: 13, b: 14, c: 49},
+]);
 
-// mconcat(['a', 'b', 'c']) = 'abc'
-// const mconcat = foldr(mappend)(empty());
+const obj2 = fmap(pick(['a', 'c']))(objs);
 
-const not = a => !a;
-
-let complement = func => compose([not, func]);
-let neq = compose([complement, eq]);
-let gt = compose([complement, lte]);
-let lt = a => b => lte(a)(b) && neq(a)(b);
-let gte = a => b => gt(a)(b) || eq(a)(b);
+console.log(JSON.stringify(obj2));
