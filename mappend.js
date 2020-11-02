10b611c49cba65939338d59d6c9d20b21a8d391f
wcsomega <wcsomega@gmail.com>
2020-11-02 01:12:32 -0500

    Change 'mappend' to try to wrap arrays before coercing pure/empty values. Add Any, All, Sum, and Product types that each implement mappend differently.

--- mappend.js
+++ mappend.js
@@ -4,9 +4,9 @@ const { empty } = require('./empty');
 const { foldr } = require('./array');
 
 const mappend = a => b => {
-  let [_a, _b] = tryCoerceEmptyOrPure(a, b);
-  _a = tryWrapArray(_a);
-  _b = tryWrapArray(_b);
+  let _a = tryWrapArray(a);
+  let _b = tryWrapArray(b);
+  [_a, _b] = tryCoerceEmptyOrPure(_a, _b);
 
   if (typeof a == 'string' && typeof b == 'string') {
     return a.concat(b);
@@ -26,4 +26,42 @@ const mappend = a => b => {
 }
 const mconcat = foldr(mappend)(empty());
 
-module.exports = { mappend, mconcat }
+function All (_value) {
+  //monoid under conjunction (&&)
+  this['@@mappend@@'] = b => all(_value && value(b));
+  this['@@fromEmpty@@'] = () => all(true);
+  this['@@fromPure@@'] = a => all(a);
+  this['@@value@@'] = _value;
+}
+
+function Any (_value) {
+  //monoid under disjunction (||)
+  this['@@mappend@@'] = b => any(_value || value(b));
+  this['@@fromEmpty@@'] = () => any(false);
+  this['@@fromPure@@'] = a => any(a);
+  this['@@value@@'] = _value;
+}
+
+function Sum (_value) {
+  //monoid under addition
+  this['@@mappend@@'] = b => sum(_value + value(b));
+  this['@@fromEmpty@@'] = () => sum(0);
+  this['@@fromPure@@'] = a => sum(a);
+  this['@@value@@'] = _value;
+}
+
+function Product (_value) {
+  //monoid under multiplication
+  this['@@mappend@@'] = b => product(_value * value(b));
+  this['@@fromEmpty@@'] = () => product(1);
+  this['@@fromPure@@'] = a => product(a);
+  this['@@value@@'] = _value;
+}
+
+const value = a => a['@@value@@'];
+const all = a => new All(a);
+const any = a => new Any(a);
+const sum = a => new Sum(a);
+const product = a => new Product(a);
+
+module.exports = { mappend, mconcat, all, any, sum, product }
